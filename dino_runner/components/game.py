1c578eb62ff6dcf194bb1ccb1e683130e6a30c9c
Airy-g <126730690+Airy-g@users.noreply.github.com>
2023-03-02 20:12:50 -0600

    Class 1: create dinosaur

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -8,10 +8,10 @@ class Game:
         pygame.init()
         pygame.display.set_caption(TITLE)
         pygame.display.set_icon(ICON)
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) ##Valor especial constante(())
         self.clock = pygame.time.Clock()
-        self.playing = False
-        self.game_speed = 20
+        self.playing = False 
+        self.game_speed = 20 
         self.x_pos_bg = 0
         self.y_pos_bg = 380
 
@@ -35,7 +35,7 @@ class Game:
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
-        self.draw_background()
+        self.draw_background() #investigar eso  draw_backgroground
         pygame.display.update()
         pygame.display.flip()
 
@@ -43,7 +43,7 @@ class Game:
         image_width = BG.get_width()
         self.screen.blit(BG, (self.x_pos_bg, self.y_pos_bg))
         self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
-        if self.x_pos_bg <= -image_width:
+        if self.x_pos_bg <= -image_width:##Tarea investigar esto 
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
-            self.x_pos_bg = 0
+            self.x_pos_bg = 0 ##
         self.x_pos_bg -= self.game_speed
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from dino_runner.components.game import Game
-
+from dino_runner.components.game import Game 
+##Carpetas que estamos extrayendo, se dividen en el punto.
 if __name__ == "__main__":
     game = Game()
     game.run()
